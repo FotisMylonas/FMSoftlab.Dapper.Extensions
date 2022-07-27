5a328c6d568c20dc019782639687849b84ead564
Fotis Mylonas <fmylontech@hotmail.com>
2022-07-27 18:35:25 +0300

    updates

--- FMSoftlab.Dapper.Extensions.Tests/UnitTest1.cs
+++ FMSoftlab.Dapper.Extensions.Tests/UnitTest1.cs
@@ -12,10 +12,38 @@ namespace FMSoftlab.Dapper.Extensions.Tests
         [MaxLength(5)]
         public string Id { get; set; }
     }
+
+    public class DapperCallParameters
+    {
+        public object IdList { get; set; }
+    }
     public class UnitTest1
     {
         [Fact]
         public void Test1()
+        {
+            using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDb)\MSSQLLocalDB; Initial Catalog=tempdb; Trusted_Connection=True;"))
+            {
+                con.Open();
+                try
+                {
+                    con.Execute("create type dbo.idlist as table (id nvarchar(5))");
+                    DynamicParameters dyn = new DynamicParameters();
+                    IEnumerable<ListItem> idlist = new List<ListItem>() { new ListItem { Id = "1" }, new ListItem { Id = "2" } };
+                    dyn.Add("idlist", idlist.GetTableValuedParam<ListItem>("dbo.idlist"));
+                    var results = con.Query<string>(@"select id from @idlist idlist", dyn).AsList<string>();
+                    Assert.Equal("1", results[0]);
+                    Assert.Equal("2", results[1]);
+                }
+                finally
+                {
+                    con.Execute("drop type dbo.idlist");
+                }
+            }
+        }
+
+        [Fact]
+        public void Test2()
         {
             using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDb)\MSSQLLocalDB; Initial Catalog=tempdb; Trusted_Connection=True;"))
             {
@@ -36,5 +64,30 @@ namespace FMSoftlab.Dapper.Extensions.Tests
                 }
             }
         }
+
+        [Fact]
+        public void Test3()
+        {
+            using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDb)\MSSQLLocalDB; Initial Catalog=tempdb; Trusted_Connection=True;"))
+            {
+                con.Open();
+                try
+                {
+                    con.Execute("create type dbo.idlist as table (id nvarchar(5))");
+                    IEnumerable<ListItem> idlist = new List<ListItem>() { new ListItem { Id = "1" }, new ListItem { Id = "2" } };
+                    var parameters = new
+                    {
+                        IdList = idlist.GetTableValuedParam<ListItem>("dbo.idlist")
+                    };
+                    var results = con.Query<string>(@"select id from @idlist idlist", parameters).AsList<string>();
+                    Assert.Equal("1", results[0]);
+                    Assert.Equal("2", results[1]);
+                }
+                finally
+                {
+                    con.Execute("drop type dbo.idlist");
+                }
+            }
+        }     
     }
 }
